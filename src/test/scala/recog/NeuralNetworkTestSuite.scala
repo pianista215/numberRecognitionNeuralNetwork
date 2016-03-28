d6073229bf624fd3b39efd604f573f29c2f636f6
Unai Sarasola <usarasola@stratio.com>
2016-03-28 13:25:56 +0200

    Test with neural network of two layers

--- src/test/scala/recog/NeuralNetworkTestSuite.scala
+++ src/test/scala/recog/NeuralNetworkTestSuite.scala
@@ -41,4 +41,29 @@ class NeuralNetworkTestSuite extends FunSuite{
     assert(network(input) == List(4.0))
   }
   
+  test("Neural Network (2 - 2) with 2 inputs") {
+    val theta11 = List(0.0, 1.0, 1.0)
+    val theta12 = List(0.0, 1.0, 2.0)
+    
+    val input = List(2.0, 6.0)
+    
+    val neuron11 = Neuron(theta11)
+    val neuron12 = Neuron(theta12)
+    
+    val theta21 = List(0.0, 1.0, 0.5)
+    val theta22 = List(0.0, 1.0, 2.0)
+    val neuron21 = Neuron(theta21)
+    val neuron22 = Neuron(theta22)
+    
+    assert(neuron11.apply(1.0::input) === 8.0)
+    assert(neuron12.apply(1.0::input) === 14.0)
+    
+    assert(neuron21.apply(1.0::List(8.0,14.0)) === 15.0)
+    assert(neuron22.apply(1.0::List(8.0,14.0)) === 36.0)
+    
+    val network = NeuralNetwork(List(neuron11,neuron12), List(neuron21, neuron22))
+    assert(network.apply(input) === List(15.0, 36.0))
+    
+  }
+  
 }
